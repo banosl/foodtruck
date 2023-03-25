e98cfc9b64735843dc843d16c6fd1351246f7ee7
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-25 07:01:28 -0600

    feat: Add validations and testing for events, tests passing

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+  validates_presence_of :food_truck_id,
+                        :event_date,
+                        :latitude,
+                        :longitude,
+                        :start_time,
+                        :end_time,
+                        :description
+                        
   belongs_to :food_truck
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  it { should belong_to(:food_truck) }
+  describe "associations" do
+    it { should belong_to(:food_truck) }
+  end
 
+  describe "validations" do
+    it { should validate_presence_of :food_truck_id }
+    it { should validate_presence_of :event_date }
+    it { should validate_presence_of :latitude }
+    it { should validate_presence_of :longitude }
+    it { should validate_presence_of :start_time }
+    it { should validate_presence_of :end_time }
+    it { should validate_presence_of :description }
+  end
 end
