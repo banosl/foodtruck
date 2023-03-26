905133d860e4fa4296ef8b3305cc835081b2332f
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-26 15:49:22 -0600

    Starts to add Foodtruck PORO

---
+++ app/poros/foodtruck.rb
@@ -0,0 +1,12 @@
+class Foodtruck
+  
+  def initialize(data)
+    @name = data[:name]
+    @image_url = data[:image_url]
+    @url
+    @latitude
+    @longitude
+    @address
+    @phone
+  end
+end
