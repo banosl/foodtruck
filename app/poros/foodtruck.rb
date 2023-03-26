f48c1c747e0929c7bdcd75761c42170346bc473f
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-26 17:26:52 -0600

    adds instance variables for Foodtruck and #format_address

--- app/poros/foodtruck.rb
+++ app/poros/foodtruck.rb
@@ -1,12 +1,22 @@
 class Foodtruck
-  
+  attr_reader :name,
+              :image_url,
+              :url,
+              :latitude,
+              :longitude,
+              :address,
+              :phone
   def initialize(data)
     @name = data[:name]
     @image_url = data[:image_url]
-    @url
-    @latitude
-    @longitude
-    @address
-    @phone
+    @url = data[:url]
+    @latitude = data[:coordinates][:latitude]
+    @longitude = data[:coordinates][:longitude]
+    @address = format_address(data[:location][:display_address])
+    @phone = data[:display_phone]
+  end
+
+  def format_address(display_address)
+    display_address.join(", ")
   end
 end
--- spec/poros/foodtruck_spec.rb
+++ spec/poros/foodtruck_spec.rb
@@ -3,48 +3,48 @@ require 'rails_helper'
 RSpec.describe Foodtruck do
   it 'foodtruck exists and has attributes' do
     data = {
-      "id": "gN8o-SG2PgCQGYrOCkkqng",
-      "alias": "liangs-thai-food-denver",
-      "name": "Liang's Thai Food",
-      "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/9_RGrE1oyWgDWu7pofvtPg/o.jpg",
-      "is_closed": false,
-      "url": "https://www.yelp.com/biz/liangs-thai-food-denver?adjust_creative=OwvDgRtTs1k3C1_VUOGMbA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=OwvDgRtTs1k3C1_VUOGMbA",
-      "review_count": 420,
-      "categories": [
+      id: "gN8o-SG2PgCQGYrOCkkqng",
+      alias: "liangs-thai-food-denver",
+      name: "Liang's Thai Food",
+      image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/9_RGrE1oyWgDWu7pofvtPg/o.jpg",
+      is_closed: false,
+      url: "https://www.yelp.com/biz/liangs-thai-food-denver?adjust_creative=OwvDgRtTs1k3C1_VUOGMbA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=OwvDgRtTs1k3C1_VUOGMbA",
+      review_count: 420,
+      categories: [
           {
-              "alias": "thai",
-              "title": "Thai"
+              alias: "thai",
+              title: "Thai"
           },
           {
-              "alias": "foodtrucks",
-              "title": "Food Trucks"
+              alias: "foodtrucks",
+              title: "Food Trucks"
           }
       ],
-      "rating": 4.5,
-      "coordinates": {
-          "latitude": 39.74521,
-          "longitude": -104.9921817
+      rating: 4.5,
+      coordinates: {
+          latitude: 39.74521,
+          longitude: -104.9921817
       },
-      "transactions": [
+      transactions: [
           "delivery"
       ],
-      "price": "$",
-      "location": {
-          "address1": "16TH St And California St",
-          "address2": "",
-          "address3": "",
-          "city": "Denver",
-          "zip_code": "80202",
-          "country": "US",
-          "state": "CO",
-          "display_address": [
+      price: "$",
+      location: {
+          address1: "16TH St And California St",
+          address2: "",
+          address3: "",
+          city: "Denver",
+          zip_code: "80202",
+          country: "US",
+          state: "CO",
+          display_address: [
               "16TH St And California St",
               "Denver, CO 80202"
           ]
       },
-      "phone": "+17208108693",
-      "display_phone": "(720) 810-8693",
-      "distance": 684.5032800923377
+      phone: "+17208108693",
+      display_phone: "(720) 810-8693",
+      distance: 684.5032800923377
     }
 
     foodtruck = Foodtruck.new(data)
