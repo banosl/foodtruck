ca1c041011f25544d182cd4b30665854b1b9146c
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-26 14:03:23 -0600

    adds testing for #self.find_place

--- spec/services/google_maps_places_service_spec.rb
+++ spec/services/google_maps_places_service_spec.rb
@@ -6,9 +6,18 @@ RSpec.describe GoogleMapsPlacesService do
       query = "Denver"
       response = GoogleMapsPlacesService.find_place(query)
 
-      expect(response).to be_successful
+      expect(response).to be_a(Hash)
+      expect(response[:candidates]).to be_a(Array)
+      expect(response[:status]).to eq("OK")
 
-      binding.pry
+      response[:candidates].each do |result|
+        expect(result).to have_key(:formatted_address)
+        expect(result).to have_key(:geometry)
+        expect(result).to have_key(:name)
+        expect(result[:geometry]).to have_key(:location)
+        expect(result[:geometry][:location]).to have_key(:lat)
+        expect(result[:geometry][:location]).to have_key(:lng)
+      end
     end
   end
 end
