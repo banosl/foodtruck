6e0c7b368dd0af69ecde292bea6b16db1b858067
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-27 11:59:25 -0600

    test: update testing for yelp_service, tests passing

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -10,18 +10,25 @@ RSpec.describe YelpService do
       longitude = -104.990251
 
       response = YelpService.get_food_trucks(latitude, longitude)
-      # require 'pry'; binding.pry
       expect(response).to have_key(:businesses)
-
+      expect(response[:businesses]).to be_a Array
       response[:businesses].each do |business|
         expect(business).to have_key(:id)
+        expect(business[:id]).to be_a String
         expect(business).to have_key(:alias)
+        expect(business[:alias]).to be_a String
         expect(business).to have_key(:name)
+        expect(business[:name]).to be_a String
         expect(business).to have_key(:image_url)
+        expect(business[:image_url]).to be_a String
         expect(business).to have_key(:url)
+        expect(business[:url]).to be_a String
         expect(business).to have_key(:categories)
+        expect(business[:categories]).to be_a Array
         expect(business).to have_key(:location)
+        expect(business[:location]).to be_a Hash
         expect(business).to have_key(:phone)
+        expect(business[:phone]).to be_a String
       end
     end
   end
